78e1599b97d4da0d5545dc14bfc16df260d6063c
Elliot Blanchard <public@elliotblanchard.com>
2020-04-22 02:20:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,8 @@ class Scraper
 
   def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      doc.css(".post")
+      doc.css(".post").first.css("h2").text
 
       binding.pry
   end
